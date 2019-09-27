0d75bef30310d6b542919edba1e220eb8145342e
Robert Sarpong <34831325+lilnop@users.noreply.github.com>
2019-09-27 12:37:08 -0700

    Add files via upload
    Updated quote app and built it in react.

---
+++ precache-manifest.f2aad7524e3cd0ace6c71f991293c70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18a84547b574cf9c9593af894775fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b156eeda9313dfc4393",
+    "url": "/static/css/main.b47a9837.chunk.css"
+  },
+  {
+    "revision": "905cea7c4e895e487b16",
+    "url": "/static/js/2.d3a1e0e5.chunk.js"
+  },
+  {
+    "revision": "9b156eeda9313dfc4393",
+    "url": "/static/js/main.c1c3de6f.chunk.js"
+  },
+  {
+    "revision": "24291f02159625329a20",
+    "url": "/static/js/runtime-main.dcd2e451.js"
+  },
+  {
+    "revision": "97186009f642856fbcba149031877985",
+    "url": "/static/media/bg.97186009.jpg"
+  }
+]);
