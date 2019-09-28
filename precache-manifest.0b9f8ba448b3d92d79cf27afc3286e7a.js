6aaa2a96b72b6ae51c5edd3ec0aa6ae93926bacd
lilnop <lilsnop@gmail.com>
2019-09-27 20:48:36 -0700

    Updates

---
+++ precache-manifest.0b9f8ba448b3d92d79cf27afc3286e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c778b94d27fa50ae8d5dde1c1a16d045",
+    "url": "/random_quote_generator/index.html"
+  },
+  {
+    "revision": "96574087f455e507e9ad",
+    "url": "/random_quote_generator/static/css/main.261ee904.chunk.css"
+  },
+  {
+    "revision": "905cea7c4e895e487b16",
+    "url": "/random_quote_generator/static/js/2.d3a1e0e5.chunk.js"
+  },
+  {
+    "revision": "96574087f455e507e9ad",
+    "url": "/random_quote_generator/static/js/main.33b92652.chunk.js"
+  },
+  {
+    "revision": "09b0866b67d9a98f46dd",
+    "url": "/random_quote_generator/static/js/runtime-main.40988d77.js"
+  },
+  {
+    "revision": "97186009f642856fbcba149031877985",
+    "url": "/random_quote_generator/static/media/bg.97186009.jpg"
+  }
+]);
